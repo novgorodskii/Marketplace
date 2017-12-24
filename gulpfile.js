a47b1faf26325524b097ed42401140fafffc2ab8
novgorodskii <novgorodskiji@gmail.com>
2017-12-24 13:38:19 +0200

    module installation

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
+const spritesmith = require('gulp.spritesmith');
+const merge = require('merge-stream');
+const tinypng = require('gulp-tinypng');
+
+
 
 gulp.task('autoprefixer', function () {
     gulp.src('src/css/**/*.css')
@@ -10,9 +15,41 @@ gulp.task('autoprefixer', function () {
         .pipe(gulp.dest('app/css'))
 } );
 
-gulp.task('watch', ['autoprefixer'], function () {
+gulp.task('sprite', function () {
+    var spriteData = gulp.src('src/sprite/*.png')
+        .pipe(spritesmith({ // Настройка спрайта
+            imgName: 'sprite.png',
+            cssName: 'sprite.css',
+            imgPath: '../img/sprite.png'
+        }));
+
+    var imgStream = spriteData.img
+        .pipe(gulp.dest('app/img/'));
+
+    var cssStream = spriteData.css
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('app/css/'));
+
+    return merge(imgStream, cssStream);
+});
+
+gulp.task('tinypng', function () {
+    gulp.src([
+        'src/img/**/*.jpg',
+        'src/img/**/*.png'
+    ])
+    .pipe(tinypng('g78CdNgaO7bJME_KixAnTy6jYll6i2ah'))
+    .pipe(gulp.dest('app/img/'));
+});
+
+gulp.task('watch', ['autoprefixer', 'sprite', 'tinypng'], function () {
 
     gulp.watch( 'src/css/**/*.css', ['autoprefixer'] );
+    gulp.watch( 'src/sprite/*.png', ['sprite'] );
+    gulp.watch( 'src/img/**/*', ['tinypng'] );
 
 });
 
